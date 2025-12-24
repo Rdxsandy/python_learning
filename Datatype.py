9e62a2643992db8f59424f6b5bd32c84620c62d9
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-25 00:45:03 +0530

    something..

---
+++ Datatype.py
@@ -0,0 +1,22 @@
+# integer float boolean 
+# print(100000)
+# print(1e308) this is a valid integer but 1e309 is inf..
+#float has also very big range..
+
+# in boolean we have true false 
+# print(True) print(False)
+# it has also complex number print(5+gj)
+# print(5+6j)
+# print((5+6j)*(3+2j)) =(3+28j)
+# print((2+2j)/(3+1j))=(0.7999999999999999+0.4j)
+# it is also doing multipilication and devision of complext number
+# we can write string in single inverted '' or "" or """string """ every one has own usecase
+
+# another data types are list tuple and sets
+# list are nothing but the array in java
+# list =[]=> [1,2,"apple",'mango']
+# tuple are ... (1,2,3,4)
+#set are .... {1,2,3,4}
+# dict we store in it key value pair ....
+# we can search in dictionary effectively.....
+# we have also thir type user define type and we would lern this in object oriented programming
