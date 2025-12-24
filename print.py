fe1ecce88a962d1759a117bf608e8b1b3bede12e
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-24 23:50:14 +0530

    your message

---
+++ print.py
@@ -0,0 +1,5 @@
+print("sundeep")
+print(1,2,3,4,5,end='\n')
+print(1,2,3,4,5,sep='--') 
+# separater separate the based on instruction third line output will be 1--2--3--4--5
+print("1",1,3,33,44.44)
