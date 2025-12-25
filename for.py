b1c1c70e22c002402908fe175de338120416fe1f
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-25 18:25:28 +0530

    forloop

---
+++ for.py
@@ -0,0 +1,41 @@
+
+# lets learn about the for loop of python
+
+#  there is range function
+
+# print(list(range(15)))=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# print(list(range(5,10)))=[5, 6, 7, 8, 9]
+# print(list(range(50,5,-5)))= [50, 45, 40, 35, 30, 25, 20, 15, 10]
+# print(list(range(1,50,5)))=[1, 6, 11, 16, 21, 26, 31, 36, 41, 46]
+
+# for i in range(1,10):
+#     print(i)
+#  output-
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         print(i*j, end=' ')
+#     print()
+# output
+# 1 2 3 4 
+# 2 4 6 8 
+# 3 6 9 12
+# 4 8 12 16
+
+# in python for loop there is no termination condition its simply iterate on sequence
+# sequence may be stirng and list and tuple 
+
+# for i in "kolkata":
+#     print(i,end=' ')
+
+# output = k o l k a t a 
+
