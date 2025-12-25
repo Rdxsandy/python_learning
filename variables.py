eebae1725ad14d334910375deeefc3a287bbbca3
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-25 12:44:36 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+# in python we do not need to define the datatype of varible bcoz it is dynamicaly typed language
+# no need to declare variable before use 
+# a ='nitish'
+# b = 5
+# static typing in which we decide the datatype at compile time
+# python also support dynamic binding in 
+
+# what is dynamic binding it is basically suppose i assinged a ='sundeep' after that i can assing a=5 it hold ture in python 
+# a,b,c,d=0,0,0,0
+# print(a,b,c,d)= 0 0 0 0
+
+# a=b=c=d=7
+# print(a,b,c,d) = 7 7 7 7
+
