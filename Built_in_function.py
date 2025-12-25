68b7742f423034112fb60b66687ac0a6dbba2b36
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-25 19:01:48 +0530

    Built_in_function

---
+++ Built_in_function.py
@@ -0,0 +1,51 @@
+# len()  
+# nums = [1, 2, 3, 4]
+# print(len(nums))=4
+
+
+# sum()
+# nums = [1, 2, 3, 4]
+# print(sum(nums))=10
+
+# max and min function
+
+# nums = [5, 2, 9, 1]
+# print(max(nums))=9
+# print(min(nums))=1
+
+# print(list(range(1, 6)))
+
+# x = 10
+# print(type(x))
+
+# a = int("10")
+# b = float("3.5")
+# c = str(25)
+# print(a, b, c)
+# ==10 3.5 25
+
+
+# sorted
+# nums = [4, 1, 3, 2]
+# print(sorted(nums))=[1, 2, 3, 4]
+
+# print(abs(-7))=7
+
+# print(round(3.6))=4
+# print(round(3.4))=3
+# print(round(3.6))
+# print(round(3.5456,2))=3.55
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ for1.py
@@ -0,0 +1,13 @@
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print("*",end=' ')
+#     print()        
+# this is the code of above pattern
+
+
