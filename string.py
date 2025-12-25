cc79dc324f66edf5ce47351e38cfe5386ed59df4
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-25 19:57:19 +0530

    stirng...

---
+++ string.py
@@ -0,0 +1,39 @@
+# in python stirngs are sequence of unicode character
+# in general we can say stirng is sequence of character
+# creation of strings
+# we can use '' or " " or ''' ''' or """ """
+# c = 'stirng'
+# c = "string"
+# c = '''string''' this is generally used for multiline string
+
+# indexing in python
+# there are two type of indexing in python negative and positive
+# c ='hello'
+# print(c[0])=h
+# print(c[1])=e  positive indexing
+
+# print(c[-1])=o negative indexing which start from end of the string last character treated as -1 
+# second last character treated as -2
+
+# slicing of strings 
+c = 'Hello world'
+
+# print(c[1:4])=ell
+# print(c[2:])=llo world
+# print(c[:4])=Hell
+
+# print(c[:-1])=Hello worl
+# print(c[-1:])=d
+
+# print(c[0:8:3])=Hlw last parameter skip the number of character
+
+# if we want to reverse the string
+
+# print(c[::-1])=dlrow olleH
+
+
+
+
+
+
+
