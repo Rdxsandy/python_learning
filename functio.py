5b3af74767a88c55e4790ceb6ca792723e7bb2b1
Rdxsandy <sundeepgangwar@gmail.com>
2025-12-31 19:13:28 +0530

    function

---
+++ functio.py
@@ -0,0 +1,36 @@
+#  write a function that a number is odd or even
+
+def is_even (a):
+    """
+    this function takes a number and 
+    give the output the number is 
+    odd or even
+   
+    """
+    if(a%2==0):
+          print("number is even")
+    else:
+          print("number is odd")
+         
+
+
+is_even(9)  
+is_even(10)
+# print(is_even.__doc__)   tells the docomentation of the function
+
+ # parameter vs argument
+ # brightness is parameter and 0 1.....9 10 is arguments 
+ # python has four type of arguments 
+ # Default argument
+ # positional argument
+#  keyword arguments  can override the  positional arguments
+ # Arbitary arguments  we can pass here tuples and it can handle manu arguments 
+#   in python we can rename a function
+#  in python we can delete a function
+# storing a function
+# returning a function
+# function as a argument
+ 
+    
+
+              
